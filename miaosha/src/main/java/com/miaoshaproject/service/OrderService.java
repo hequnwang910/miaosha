83eca9d9a462f00d1cbba3359b905db5a86cccca
whq <1739757230@qq.com>
2021-06-23 10:21:38 +0800

    fisrt commit

---
+++ miaosha/src/main/java/com/miaoshaproject/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.miaoshaproject.service;
+
+import com.miaoshaproject.erro.BusinessException;
+import com.miaoshaproject.service.model.OrderModel;
+
+public interface OrderService {
+    //1、通过前端URL上传过来秒杀活动ID，然后下单接口内校验对应id是否属于对应商品且活动已经开始 >推荐使用第一种
+    //2、直接在下单接口内判断对应的商品是否存在秒杀活动，若存在进行中的则以秒杀价格下单
+    OrderModel createOrder(Integer userId, Integer itemId, Integer promoId, Integer amount) throws BusinessException;
+
+
+
+}
---
+++ miaosha/src/main/java/com/miaoshaproject/service/UserService.java
@@ -0,0 +1,22 @@
+package com.miaoshaproject.service;
+
+import com.miaoshaproject.erro.BusinessException;
+import com.miaoshaproject.service.model.UserModel;
+
+public interface UserService {
+
+    //通过用户Id获取用户对象的方法；
+    UserModel getUserById(Integer id);
+
+    void register(UserModel userModel) throws BusinessException;
+
+
+    /**
+     * @param telphone 用户注册手机
+     * @param encrptPassword 用户加密后的密码
+     * @return
+     * @throws BusinessException
+     */
+    UserModel validateLogin(String telphone, String encrptPassword) throws BusinessException;
+
+}
